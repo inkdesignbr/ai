90e7b6769c75eae3d5a04238aa3bd2306637fe85
inkdesignbr <148729105+inkdesignbr@users.noreply.github.com>
2024-01-15 14:32:08 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,24 @@
-importar streamlit como st 
-de streamlit_chat importar mensagem 
-de streamlit_extras.colored_header importar cabeçalho colorido 
-de streamlit_extras.add_vertical_space importar add_vertical_space 
-de hugchat importar hugchat
+import streamlit as st
+from streamlit_chat import message
+from streamlit_extras.colored_header import colored_header
+from streamlit_extras.add_vertical_space import add_vertical_space
+from hugchat import hugchat
+st.set_page_config(page_title="HugChat - An LLM-powered Streamlit app")
+with st.sidebar:
+    st.title('🤗💬 HugChat App')
+    st.markdown('''
+    ## About
+    This app is an LLM-powered chatbot built using:
+    - [Streamlit](<https://streamlit.io/>)
+    - [HugChat](<https://github.com/Soulter/hugging-chat-api>)
+    - [OpenAssistant/oasst-sft-6-llama-30b-xor](<https://huggingface.co/OpenAssistant/oasst-sft-6-llama-30b-xor>) LLM model
+    
+    💡 Note: No API key required!
+    ''')
+    add_vertical_space(5)
+    st.write('Made with ❤️ by [Data Professor](<https://youtube.com/dataprofessor>)')
+if 'generated' not in st.session_state:
+    st.session_state['generated'] = ["I'm HugChat, How may I help you?"]
+
+if 'past' not in st.session_state:
+    st.session_state['past'] = ['Hi!']
